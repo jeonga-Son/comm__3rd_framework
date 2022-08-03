3d738f83e2d5f2d9622818bb568318b4ec0bd0a5
jeonga-Son <jjeong0106@gmail.com>
2022-08-04 03:12:51 +0900

    Container가 자동으로 모든 컴포넌트를 스캔하여 검색, 검색된 것들을 객체화, 저장

--- src/main/java/com/ll/exam/article/controller/ArticleController.java
+++ src/main/java/com/ll/exam/article/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.ll.exam.article.controller;
 import com.ll.exam.annotation.Controller;
 import com.ll.exam.annotation.GetMapping;
 
+// @Controller가 붙으면 new할 필요가 없다.
 @Controller // ArticleController가 컨르롤러라는 것을 알려줌.
 public class ArticleController {
     @GetMapping("/usr/article/list") // /usr/article/list/free와 같이 관련된 요청을 처리하는 함수라고 알려주는 것.
