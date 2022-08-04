640d13b978bd7867d627a9c71c81ef86286af5ea
jeonga-Son <jjeong0106@gmail.com>
2022-08-04 09:47:49 +0900

    Autowired 어노테이션 작동 하도록

--- src/main/java/com/ll/exam/Container.java
+++ src/main/java/com/ll/exam/Container.java
@@ -1,15 +1,13 @@
 package com.ll.exam;
 
+import com.ll.exam.annotation.Autowired;
 import com.ll.exam.annotation.Controller;
 import com.ll.exam.annotation.Service;
 import com.ll.exam.article.controller.ArticleController;
 import com.ll.exam.home.controller.HomeController;
 import org.reflections.Reflections;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Container {
 
@@ -24,7 +22,39 @@ public class Container {
     private static void scanComponents() {
         scanServices();
         scanControllers();
+        
+        // 조립
+        resolveDependenciesAllComponents();
     }
+
+    private static void resolveDependenciesAllComponents() {
+        for (Class cls : objects.keySet()) {
+            Object o = objects.get(cls);
+
+            resolveDependencies(o);
+        }
+    }
+
+    private static void resolveDependencies(Object o) {
+        Arrays.asList(o.getClass().getDeclaredFields())
+                .stream()
+                .filter(f -> f.isAnnotationPresent(Autowired.class))
+                .map(field -> {
+                    field.setAccessible(true);
+                    return field;
+                })
+                .forEach(field -> {
+                  Class cls = field.getType();
+                    Object dependency = objects.get(cls);
+
+                    try {
+                        field.set(o, dependency);
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+    }
+
 
     private static void scanServices() {
         Reflections ref = new Reflections("com.ll.exam");
