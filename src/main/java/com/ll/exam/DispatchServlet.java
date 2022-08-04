7c0853ede2455d4f3a4c57cdaab6fff44a84e7f8
jeonga-Son <jjeong0106@gmail.com>
2022-08-04 11:15:44 +0900

    들어오는 요청에 맞는 매핑정보를 찾는다.

--- src/main/java/com/ll/exam/DispatchServlet.java
+++ src/main/java/com/ll/exam/DispatchServlet.java
@@ -1,12 +1,9 @@
 package com.ll.exam;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-import java.io.IOException;
 
 @WebServlet("/usr/*")
 public class DispatchServlet extends HttpServlet {
