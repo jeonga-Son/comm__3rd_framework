8ebe3d400abf1c387116c54818904f56da6c5f70
jeonga-Son <jjeong0106@gmail.com>
2022-08-04 09:14:40 +0900

    ArticleService 를 컨테이너에서 얻는 테스트케이스 추가

--- src/main/java/com/ll/exam/article/controller/ArticleController.java
+++ src/main/java/com/ll/exam/article/controller/ArticleController.java
@@ -2,10 +2,14 @@ package com.ll.exam.article.controller;
 
 import com.ll.exam.annotation.Controller;
 import com.ll.exam.annotation.GetMapping;
+import com.ll.exam.article.service.ArticleService;
 
 // @Controller가 붙으면 new할 필요가 없다.
 @Controller // ArticleController가 컨르롤러라는 것을 알려줌.
 public class ArticleController {
+
+    private ArticleService articleService;
+
     @GetMapping("/usr/article/list") // /usr/article/list/free와 같이 관련된 요청을 처리하는 함수라고 알려주는 것.
     // 아래 showList는 Get /usr/article/list 으로 요청이 왔을 때 실행 되어야 하는 함수이다.
     public void showList () {
